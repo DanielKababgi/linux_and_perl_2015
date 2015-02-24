30e335dea29e1c76484d80372042ad480ebe8434
Daniel Kababgi <daniel.kababgi@web.de>
2015-02-24 15:47:36 +0100

    Searches ID und Headlines wit ">" at the start

--- fastaparser.pl
+++ fastaparser.pl
@@ -3,14 +3,17 @@
 use strict;
 use warnings;
 
-my $awesomestuff;
-
 my $filename = "fasta.txt";
 open FASTA, $filename or die "$filename $!"; 
 
 while (<FASTA>)
 {
-    print "$_"; 
+    if($_ =~ /^>(\w+)\s(\w+)/)
+    {
+	print "$_";
+    }
+
+#    print "$_"; 
 }
 
 close FASTA or die;
