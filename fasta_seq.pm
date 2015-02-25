645cb84027d34aa91a3035ad86f05c0922ca3c6f
Daniel Kababgi <daniel.kababgi@web.de>
2015-02-25 16:15:37 +0100

    awesome test, which is failing, added: set id!

--- fasta_seq.pm
+++ fasta_seq.pm
@@ -1,1 +1,20 @@
+package fasta_seq;
+
+use strict;
+use warnings;
+
+sub new
+{
+    my $self = {};
+    bless $self,"fasta_seq";
+    return $self;
+}
+
+sub id
+{
+    my $self = {};
+    return "ralph";
+}
+
+1;
 
--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -10,6 +10,7 @@ my $ralph = new_ok ("fasta_seq");
 
 can_ok ("fasta_seq", "id");
 is ($ralph->id, "ralph", "get_id");
-
-
+$ralph -> id("willi");
+is($ralph-> id, "willi", "set_id");
+ 
 done_testing();
