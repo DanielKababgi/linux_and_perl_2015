8f9f91d72fb661a63e21504791f5d671d23fe3c6
Daniel Kababgi <daniel.kababgi@web.de>
2015-02-25 15:52:20 +0100

    again new tests

--- test_fasta_seq.pl
+++ test_fasta_seq.pl
@@ -2,8 +2,14 @@
 
 use strict;
 use warnings;
-use Test::More tests =>2;
+use Test::More;
 
 require_ok( 'fasta_seq' );
 
-my $ralph = new_ok ( "fasta_seq");
+my $ralph = new_ok ("fasta_seq");
+
+can_ok ("fasta_seq", "id");
+
+
+
+done_testing();
